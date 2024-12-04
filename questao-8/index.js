fad2e97c1580d958e409ddb78ac2c1279fbb1ff8
motadv <rmsodre@id.uff.br>
2024-12-04 10:19:36 -0300

    Teste no projeto de servidor

--- questao-8/index.js
+++ questao-8/index.js
@@ -1,6 +1,7 @@
 const app = require("./setup");
 const port = 3000;
 
+// Inicia o servidor na porta especificada
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
